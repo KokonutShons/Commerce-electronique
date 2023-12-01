354adb633bcd4dadb4c57136be23952078a87989
KokonutShons <seanfujiwara2001@gmail.com>
2023-12-01 02:00:57 -0800

    Update payment review in final payment page step

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Typography, Button, Divider } from '@material-ui/core';
+import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js';
+import { loadStrip } from '@stripe/stripe-js';
+
+import Review from './Review'
 
 const PaymentForm = () => {
     return (
-        <div>
-            PaymentForm
-        </div>
+        <>
+            <Review checkoutToken={checkoutToken}/>
+        </>
     );
 }
 
-export default PaymentForm
+export default PaymentForm;
